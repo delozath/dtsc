e24497f9d1e409fda518960815afd3c97075e5dd
delozath@gmail.com <delozath@gmail.com>
2022-04-07 11:03:42 -0500

    Purged imports in feature.py

--- data/features.py
+++ data/features.py
@@ -1,11 +1,10 @@
 import pdb
 
-import numpy as np 
 
 class cvars():
     """docstring for ."""
 
-    def __init__(self,db,group):
+    def __init__(self, db, group):
         self.KEY        = 'key'
         self.FEATURES   = 'feature'
         self.TARTETS    = 'target'
@@ -14,15 +13,15 @@ class cvars():
         self.ALL        = 'all'
         self.USELESS    = 'useless'
         self.table      = 'features'
-        
-        self.db         = db
-        self.group      = group
-        
+
+        self.db    = db
+        self.group = group
+
         self._query_df_columns()
-    
-    def update_colnames(self,cols):
+
+    def update_colnames(self, cols):
         #cols -> dict {old_name:new_name}
-        for key,values in cols.items():
+        for key, values in cols.items():
             tmp = [values]
             if key in self.keys:
                 self.keys.remove(key)
@@ -37,11 +36,11 @@ class cvars():
                 self.treatments.remove(key)
                 self.treatments = self.treatments + tmp
             else:
-                raise ValueError ('Not defined column error')
-            
+                raise ValueError('Not defined column error')
+
             self.all = self.keys + self.features + self.targets + self.treatments
-    
-    def drop_colnames(self,cols):
+
+    def drop_colnames(self, cols):
         for col in cols:
             if col in self.keys:
                 self.keys.remove(col)
@@ -52,28 +51,32 @@ class cvars():
             elif col in self.treatments:
                 self.treatments.remove(col)
             else:
-                raise ValueError ('Not defined column error')
-            
+                raise ValueError('Not defined column error')
+
             self.all = self.keys + self.features + self.targets + self.treatments
-    
+
     def _query_df_columns(self):
-        query                  = self.db[self.table].query("group=='%s'"%self.group)
-        
-        self.keys       = query[query['type']==self.KEY       ][self.FEATURES].to_list()
-        self.features   = query[query['type']==self.FEATURES  ][self.FEATURES].to_list()
-        self.targets    = query[query['type']==self.TARTETS   ][self.FEATURES].to_list()
-        self.treatments = query[query['type']==self.TREATMENTS][self.FEATURES].to_list()
-        self.useless    = query[query['type']==self.USELESS   ][self.FEATURES].to_list()
-        self.all        = self.keys + self.features + self.targets + self.treatments
-        
+        query = self.db[self.table].query("group=='%s'" % self.group)
+
+        self.keys = query[query['type'] == self.KEY][self.FEATURES].to_list()
+        self.features = query[query['type']
+                              == self.FEATURES][self.FEATURES].to_list()
+        self.targets = query[query['type']
+                             == self.TARTETS][self.FEATURES].to_list()
+        self.treatments = query[query['type']
+                                == self.TREATMENTS][self.FEATURES].to_list()
+        self.useless = query[query['type']
+                             == self.USELESS][self.FEATURES].to_list()
+        self.all = self.keys + self.features + self.targets + self.treatments
+
         #cluster de variables
         #TODO try-except cuando no hay columna cluster
-        df              = query[query['type']!='useless']
-        clusters        = {i:j['feature'].to_list() for i,j in df.groupby('cluster')}
-        self.clusters   = clusters
-    
+        df = query[query['type'] != 'useless']
+        clusters = {i: j['feature'].to_list()
+                    for i, j in df.groupby('cluster')}
+        self.clusters = clusters
+
     #TODO parametro para especificar que tipo de variable se modificara
     #en esta version se quitan todos los espacios de todas las columnas
     def drop_spaces_cnames(self, crep='_'):
-        
         pdb.set_trace()
