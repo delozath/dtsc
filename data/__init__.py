d160222faddede76df0ff2e5675c99d46c14ffb4
delozath@gmail.com <delozath@gmail.com>
2022-07-20 14:50:14 -0500

    full_context_vars_include bug corrected

--- data/__init__.py
+++ data/__init__.py
@@ -1,6 +1,5 @@
 #from dtsc.data           import tables
 from dtsc.data.load      import load_table
-from dtsc.data.load      import Load_File
 from dtsc.data.load      import load_full_context
 from dtsc.data.save      import export_dataframe
 from dtsc.data.save      import Save_File
